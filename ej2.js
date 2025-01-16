29a1360068253fb155585d6850fda87aae49b2c4
gezcarlos22 <ceg290698@gmail.com>
2025-01-15 21:01:05 -0300

    *Agrego ejercicio 2 de JS

---
+++ ej2.js
@@ -0,0 +1,11 @@
+var nombre;
+var apellido;
+var edad;
+var fechaDeNacimiento;
+var direccion;
+
+nombre = "Carlos";
+apellido = "Gez";
+edad = "26",
+fechaDeNacimiento = "29/06/1998";
+direccion = "General artigas";
